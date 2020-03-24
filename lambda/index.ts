b661869be9c2a160b1155589ab09180bce3d5012
Daniel Szenasi <daniel.szenasi@supercharge.io>
2020-03-24 22:55:57 +0100

    feat: add slack bolt

--- lambda/index.ts
+++ lambda/index.ts
@@ -1,8 +1,79 @@
-export const handler = async (event: any = {}): Promise<any> => {
-  console.log('request:', JSON.stringify(event, undefined, 2));
-  return {
-    statusCode: 200,
-    headers: { 'Content-Type': 'text/plain' },
-    body: `Hello, CDK! You've hit ${event.path}\n`
-  };
+import * as awsServerlessExpress from 'aws-serverless-express';
+import { Application } from 'express';
+import { App, ExpressReceiver } from '@slack/bolt';
+
+const expressReceiver = new ExpressReceiver({
+  signingSecret: process.env.SLACK_SIGNING_SECRET!
+});
+export const expressApp: Application = expressReceiver.app;
+
+const app: App = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  receiver: expressReceiver
+});
+
+app.event('app_home_opened', async ({ event, context }) => {
+  try {
+    /* view.publish is the method that your app uses to push a view to the Home tab */
+    const result = await app.client.views.publish({
+      /* retrieves your xoxb token from context */
+      token: context.botToken,
+
+      /* the user that opened your app's app home */
+      user_id: event.user,
+
+      /* the view payload that appears in the app home*/
+      view: {
+        type: 'home',
+        callback_id: 'home_view',
+
+        /* body of the view */
+        blocks: [
+          {
+            type: 'section',
+            text: {
+              type: 'mrkdwn',
+              text: "*Welcome to your _App's Home_* :tada:"
+            }
+          },
+          {
+            type: 'divider'
+          },
+          {
+            type: 'section',
+            text: {
+              type: 'mrkdwn',
+              text:
+                "This button won't do much for now but you can set up a listener for it using the `actions()` method and passing its unique `action_id`. See an example in the `examples` folder within your Bolt app."
+            }
+          },
+          {
+            type: 'actions',
+            elements: [
+              {
+                type: 'button',
+                text: {
+                  type: 'plain_text',
+                  text: 'Click me!'
+                }
+              }
+            ]
+          }
+        ]
+      }
+    });
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+// Check the details of the error to handle cases where you should retry sending a message or stop the app
+app.error(error => {
+  console.error(error);
+});
+
+const server = awsServerlessExpress.createServer(expressApp);
+
+exports.handler = (event: any = {}, context: any = {}) => {
+  awsServerlessExpress.proxy(server, event, context);
 };
--- lib/lambda-stack.ts
+++ lib/lambda-stack.ts
@@ -1,6 +1,6 @@
 import codedeploy = require('@aws-cdk/aws-codedeploy');
 import lambda = require('@aws-cdk/aws-lambda');
-import { App, Stack, StackProps } from '@aws-cdk/core';
+import { App, Stack, StackProps, SecretValue } from '@aws-cdk/core';
 import * as apigw from '@aws-cdk/aws-apigateway';
 
 export class LambdaStack extends Stack {
@@ -13,7 +13,15 @@ export class LambdaStack extends Stack {
     const func = new lambda.Function(this, 'Lambda', {
       code: this.lambdaCode,
       handler: 'index.handler',
-      runtime: lambda.Runtime.NODEJS_10_X
+      runtime: lambda.Runtime.NODEJS_10_X,
+      environment: {
+        SLACK_SIGNING_SECRET: SecretValue.secretsManager(
+          'slack-signing-secret'
+        ).toString(),
+        SLACK_BOT_TOKEN: SecretValue.secretsManager(
+          'slack-bot-token'
+        ).toString()
+      }
     });
 
     const version = func.addVersion(new Date().toISOString());
