ac30fbf2874123afbe6ea8692ecc203c09d23e12
Daniel Szenasi <daniel.szenasi@supercharge.io>
2020-03-24 19:20:50 +0100

    first commit

--- bin/weekly-challenge.ts
+++ bin/weekly-challenge.ts
@@ -1,7 +1,14 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from '@aws-cdk/core';
-import { WeeklyChallengeStack } from '../lib/weekly-challenge-stack';
 
-const app = new cdk.App();
-new WeeklyChallengeStack(app, 'WeeklyChallengeStack');
+import { App } from '@aws-cdk/core';
+import { LambdaStack } from '../lib/lambda-stack';
+import { PipelineStack } from '../lib/pipeline-stack';
+
+const app = new App();
+
+const lambdaStack = new LambdaStack(app, 'LambdaStack');
+new PipelineStack(app, 'PipelineDeployingWeeklyChallengeStack', {
+  lambdaCode: lambdaStack.lambdaCode
+});
+
+app.synth();
